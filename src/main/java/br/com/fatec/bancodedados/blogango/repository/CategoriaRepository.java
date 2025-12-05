47916e7a7702a670f1bfc31609c4ef97f4e22055
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-12-05 16:48:44 -0300

    fix: remove findBySlug em categoria

--- src/main/java/br/com/fatec/bancodedados/blogango/repository/CategoriaRepository.java
+++ src/main/java/br/com/fatec/bancodedados/blogango/repository/CategoriaRepository.java
@@ -4,5 +4,4 @@ import br.com.fatec.bancodedados.blogango.model.Categoria;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CategoriaRepository extends MongoRepository<Categoria, String> {
-    int findBySlug(String slug);
 }
